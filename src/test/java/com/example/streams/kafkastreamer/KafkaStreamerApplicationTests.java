c9e3a9142b2c37481cfe9a0600bcf8d5e19658d4
bbodoque <bbodoque@belikesoftware.com>
2020-02-21 12:52:57 +0100

    Create project for learn spring cloud stream

---
+++ src/test/java/com/example/streams/kafkastreamer/KafkaStreamerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.streams.kafkastreamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamerApplicationTests {
+
+  @Test
+  void contextLoads() {
+
+  }
+
+}
