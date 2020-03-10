d4350862f41a02d7e0926703a5d72548d974dd95
bbodoque <bbodoque@belikesoftware.com>
2020-03-10 14:48:24 +0100

    Create contract for uppercase binder destination

--- src/test/java/com/example/streams/kafkastreamer/KafkaStreamerApplicationTests.java
+++ src/test/java/com/example/streams/kafkastreamer/KafkaStreamerApplicationTests.java
@@ -1,14 +1,21 @@
 package com.example.streams.kafkastreamer;
 
-import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.verifier.messaging.MessageVerifier;
+import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@ActiveProfiles("test")
 @SpringBootTest
-class KafkaStreamerApplicationTests {
+@RunWith(SpringRunner.class)
+@AutoConfigureMessageVerifier
+@EnableBinding(KafkaStreamerApplication.class)
+public abstract class KafkaStreamerApplicationTests {
 
-  @Test
-  void contextLoads() {
-
-  }
-
+  @Autowired
+  private MessageVerifier verifier;
 }
