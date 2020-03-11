77b6f08bde558cd29f715dae61b466b3a23199a0
Titos Dragonas <titos@linkener.com>
2020-03-11 17:27:28 +0100

    converted to use spring-cloud-function

--- src/main/java/com/example/streams/kafkastreamer/KafkaStreamerApplication.java
+++ src/main/java/com/example/streams/kafkastreamer/KafkaStreamerApplication.java
@@ -1,25 +1,50 @@
 package com.example.streams.kafkastreamer;
 
-import com.example.streams.kafkastreamer.vo.ValueVO;
-import io.cloudevents.v1.CloudEventBuilder;
-import io.cloudevents.v1.CloudEventImpl;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.UUID;
 import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.streams.kafkastreamer.vo.ValueVO;
+import com.github.javafaker.Faker;
+
+import io.cloudevents.v1.CloudEventBuilder;
+import io.cloudevents.v1.CloudEventImpl;
+import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
+@RestController
 public class KafkaStreamerApplication {
 
+  private final EmitterProcessor<Message<String>> processor = EmitterProcessor.create();
+	
   public static void main(String[] args) {
 
     SpringApplication.run(KafkaStreamerApplication.class, args);
   }
+  
+  @PostMapping(value="/")
+  public void handlePost() {
+	  Faker faker = new Faker();
+	  Message<String> message = MessageBuilder.withPayload(faker.name().fullName()).build();
+	  processor.onNext(message);
+  }
 
+  @Bean
+  public Supplier<Flux<Message<String>>> supplier() {
+	  return () -> processor;
+  }
+  
   @Bean
   public Function<String, CloudEventImpl<ValueVO>> uppercase() {
 
