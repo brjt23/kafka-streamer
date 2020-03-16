ad3b1bd74f62c659a7fe33cfd192f51b8ee59e3a
bbodoque <bbodoque@belikesoftware.com>
2020-03-16 15:39:25 +0100

    Remove unused libraries

--- src/main/java/com/example/streams/kafkastreamer/controller/DummyController.java
+++ src/main/java/com/example/streams/kafkastreamer/controller/DummyController.java
@@ -1,7 +1,6 @@
 package com.example.streams.kafkastreamer.controller;
 
 import com.example.streams.kafkastreamer.config.EventSupplier;
-import com.github.javafaker.Faker;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,8 +11,7 @@ public class DummyController {
   @PostMapping(value = "/")
   public void handlePost() {
 
-    Faker faker = new Faker();
-    Message<String> message = MessageBuilder.withPayload(faker.chuckNorris().fact()).build();
+    Message<String> message = MessageBuilder.withPayload("Hello World!").build();
     EventSupplier.processor.onNext(message);
   }
 }
