4e8c2e1dc552aaa6a4c917e2a68ab125e2671660
bbodoque <bbodoque@belikesoftware.com>
2020-03-16 15:36:33 +0100

    Remove unnused imports

--- src/test/java/com/example/streams/kafkastreamer/KafkaStreamerApplicationTests.java
+++ src/test/java/com/example/streams/kafkastreamer/KafkaStreamerApplicationTests.java
@@ -1,17 +1,12 @@
 package com.example.streams.kafkastreamer;
 
 import com.example.streams.kafkastreamer.controller.DummyController;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.verifier.messaging.MessageVerifier;
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
-import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.function.Supplier;
 
 @ActiveProfiles("test")
 @SpringBootTest
